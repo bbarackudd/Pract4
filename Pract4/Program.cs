398edc2ae3cf82b87fa3378f39e04d25ce9eb0f7
unknown <c468aa777@gmail.com>
2022-11-07 21:30:55 +0300

    алвыолаоы

---
+++ Pract4/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Pract4
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            int position = 1;
+            Console.SetCursorPosition(0, position);
+            Console.Write("-> ");
+
+            while (true)
+            {
+               
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    position--;
+                    Console.Clear();
+                    Console.SetCursorPosition(0, position);
+                    Console.WriteLine("-> ");
+
+                }
+                else if (key.Key == ConsoleKey.DownArrow)
+                {
+                    position++;
+                    Console.Clear();
+                    Console.SetCursorPosition(0, position);
+                    Console.Write("-> ");
+                }
+                if (position == 0)
+                {
+                    position = 1;
+                    Console.Clear();
+                    Console.SetCursorPosition(0, position);
+                    Console.WriteLine("-> ");
+                }
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    if (position == 1)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        static void zametka()
+        {
+      
+        }
+
+
+
+
+    }
+}
